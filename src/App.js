96721761b7150ba5154895b0fd6bd181fcd14e4c
HACHEM WAFAA <wafaahachem1431@gmail.com>
2025-08-01 22:41:24 +0100

    Add ChatMessages component in separate file and setup event handlers

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import ChatInput from "./ChatInput";
-import ChatMessage from "./ChatMessage";
+
+import ChatMessages from "./ChatMessages";
 
 function App() {
   return (
@@ -9,45 +10,6 @@ function App() {
         <ChatMessages />
       </>
     </div>
-  );
-}
-
-function ChatMessages() {
-  const chatMessages = [
-    {
-      id: "id1",
-      message: "hello Chatbot",
-      sender: "user",
-    },
-    {
-      id: "id2",
-      message: "Hello! How can I help you?",
-      sender: "robot",
-    },
-    {
-      id: "id3",
-      message: "can you get me todays date?",
-      sender: "user",
-    },
-    {
-      id: "id4",
-      message: "Today is August 01",
-      sender: "robot",
-    },
-  ];
-
-  return (
-    <>
-      {chatMessages.map((chatMessage) => {
-        return (
-          <ChatMessage
-            message={chatMessage.message}
-            sender={chatMessage.sender}
-            key={chatMessage.id}
-          />
-        );
-      })}
-    </>
   );
 }
 
---
+++ src/ChatMessages.js
@@ -0,0 +1,52 @@
+import ChatMessage from "./ChatMessage";
+
+function ChatMessages() {
+  const chatMessages = [
+    {
+      id: "id1",
+      message: "hello Chatbot",
+      sender: "user",
+    },
+    {
+      id: "id2",
+      message: "Hello! How can I help you?",
+      sender: "robot",
+    },
+    {
+      id: "id3",
+      message: "can you get me todays date?",
+      sender: "user",
+    },
+    {
+      id: "id4",
+      message: "Today is August 01",
+      sender: "robot",
+    },
+  ];
+
+  function sendMessage() {
+    chatMessages.push({
+      message: "test",
+      sender: "user",
+      id: crypto.randomUUID(),
+    });
+    console.log(chatMessages);
+  }
+
+  return (
+    <>
+      <button onClick={sendMessage}>Send message</button>
+      {chatMessages.map((chatMessage) => {
+        return (
+          <ChatMessage
+            message={chatMessage.message}
+            sender={chatMessage.sender}
+            key={chatMessage.id}
+          />
+        );
+      })}
+    </>
+  );
+}
+
+export default ChatMessages;
